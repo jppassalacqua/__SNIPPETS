d9cd23c1898647d1e0df5f5dafe039f3df71ecfc
jppassalacqua <12597013+jppassalacqua@users.noreply.github.com>
2023-09-06 15:07:06 +0200

    Create ui_pages_jppTestKbToWord.js

---
+++ ui_pages_jppTestKbToWord.js
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<j:jelly trim="false" xmlns:j="jelly:core" xmlns:g="glide" xmlns:j2="null" xmlns:g2="null">
+<button onclick="Export2Word('targetList', 'word-content.docx');">Export as .docx</button>
+<div id="targetList"></div>
+<script>
+var kbs = new GlideRecord('kb_knowledge');
+kbs.orderBy('number');
+kbs.query();
+var list = "";
+while (kbs.next()) {
+	list += ("<h2>"+kbs.number+":"+kbs.short_description+"</h2>");
+	list +=  ("<p>"+kbs.text+"</p><br></br>");
+}
+document.getElementById('targetList').innerHTML = list ;
+</script>
+<script>
+function Export2Word(element, filename = ''){
+    var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></meta></head><body>";
+    var postHtml = "</body></html>";
+    var html = preHtml+document.getElementById(element).innerHTML+postHtml;
+
+    var blob = new Blob(['\ufeff', html], {
+        type: 'application/msword'
+    });
+    
+    // Specify link url
+    var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+    
+    // Specify file name
+    filename = filename?filename+'.doc':'document.doc';
+    
+    // Create download link element
+    var downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob ){
+        navigator.msSaveOrOpenBlob(blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = url;
+        
+        // Setting the file name
+        downloadLink.download = filename;
+        
+        //triggering the function
+        downloadLink.click();
+    }
+    
+    document.body.removeChild(downloadLink);
+}
+</script>
+</j:jelly>
